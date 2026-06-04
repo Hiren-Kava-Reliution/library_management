f759d33b67b1f5eb9d858f63031751e063da2b69
Hiren-Kava-Reliution <hirenk.reliution@gmail.com>
2026-06-04 18:04:09 +0530

    Delete library_management directory

--- library_management/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import library_book
-from . import library_book_issue
-from . import library_member
--- library_management/models/library_member.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class LibraryMember(models.Model):
-    _name = 'library.member'
-    _description = 'Library Member'
-
-    name = fields.Char(string='Member Name')
-    email = fields.Char(string='Email Address')
-    is_active = fields.Boolean()
